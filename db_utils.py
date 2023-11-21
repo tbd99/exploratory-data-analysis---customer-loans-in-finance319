ec7623d51f08ba5fe16a5577f1126dc72b8b54b7
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-11-21 17:58:00 +0000

    rearranges file structure for clarity

--- db_utils.py
+++ db_utils.py
@@ -1,3 +1,8 @@
+import yaml
+with open('do_not_track/credentials.yaml') as f:
+    animals = yaml.safe_load(f)
+
+print(animals)
 class RDSDatabaseConnector():
     '''
     This class contains the methods used to extract data from the RDS
