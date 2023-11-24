4fca58cb955f2a0d0f691aab07c8c2221915bf13
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-11-24 10:56:14 +0000

    Identifies the columns containing null values

--- db_transform.py
+++ db_transform.py
@@ -5,8 +5,12 @@ from db_info import DataFrameInfo
 from db_info import read_csv
 
 class Plotter():
+   '''
+   '''
    
 class DataFrameTransform():
+   '''
+   '''
    
 
 if __name__ == "__main__":
@@ -14,5 +18,12 @@ if __name__ == "__main__":
    loan_payments_df = read_csv(filename)  # calls the read_csv funciton to load data 
    my_instance = DataFrameInfo(loan_payments_df) # initialises an instnce of the class 
    column_names = loan_payments_df.columns.tolist() # creates a list of the column headings as strings 
-   dtypes = my_instance.get_datatypes()   
-   print(loan_payments_df.head())
+   null_columns = []
+   print(type(null_columns))
+   for i in range (0, len(column_names)):
+      null_pc = my_instance.null_percentage(column_names[i])
+      if null_pc > 0.0:
+        null_columns.append(column_names[i])
+   print(null_columns)
+
+   
