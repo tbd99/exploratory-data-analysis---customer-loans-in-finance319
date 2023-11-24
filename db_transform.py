414f299c4a8ee74ad302658c718a2b17a958716f
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-11-24 10:45:35 +0000

    Creates the Plotter and DataFrameTranform classes

--- db_transform.py
+++ db_transform.py
@@ -2,14 +2,17 @@ import numpy as np
 import pandas as pd
 from datetime import datetime 
 from db_info import DataFrameInfo
+from db_info import read_csv
 
-def read_csv(filename):
-    df_csv = pd.read_csv(filename)
-    return df_csv 
+class Plotter():
+   
+class DataFrameTransform():
+   
 
-filename = 'loan_payments_transformed.csv'
-loan_payments_df = read_csv(filename)  # calls the read_csv funciton to load data 
-my_instance = DataFrameInfo(loan_payments_df) # initialises an instnce of the class 
-column_names = loan_payments_df.columns.tolist() # creates a list of the column headings as strings 
-dtypes = my_instance.get_datatypes()   
-print(loan_payments_df.head())
+if __name__ == "__main__":
+   filename = 'loan_payments_transformed.csv'
+   loan_payments_df = read_csv(filename)  # calls the read_csv funciton to load data 
+   my_instance = DataFrameInfo(loan_payments_df) # initialises an instnce of the class 
+   column_names = loan_payments_df.columns.tolist() # creates a list of the column headings as strings 
+   dtypes = my_instance.get_datatypes()   
+   print(loan_payments_df.head())
