0b436bfbc9d49e30ed31118812c8b4adbe25de86
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-11-24 12:45:15 +0000

    Drops rows containing null values from columns with <1% null values

--- db_transform.py
+++ db_transform.py
@@ -16,7 +16,17 @@ class DataFrameTransform():
       self.dataframe = df 
       
    def drop_column(self,column):
+      '''
+      This function deletes a specified column from the dataframe
+      '''
       self.dataframe = self.dataframe.drop(column, axis =1)
+      return self.dataframe
+   
+   def drop_null_rows(self,column):
+      '''
+      This function deletes all rows containing null values in the specified column
+      '''
+      self.dataframe = self.dataframe.dropna(subset=[column])
       return self.dataframe
    
    
@@ -55,20 +65,32 @@ if __name__ == "__main__":
       if null_pc > 0.0:
         null_columns.append(column_names[i])
    print(null_columns)
-   #loan_payments_df.info()
-   columns_to_drop = ['mths_since_last_delinq','mths_since_last_record','next_payment_date','mths_since_last_major_derog']
-   columns_to_impute = ['funded_amount','term','int_rate','employment_length','collections_12_mths_ex_med',]
-   columns_to_drop_null_value_rows = ['last_payment_date','last_credit_pull_date']
-
+   
+   columns_to_drop = ['mths_since_last_delinq','mths_since_last_record','next_payment_date','mths_since_last_major_derog'] # columns with > 50% null values, drop entire column
+   columns_to_impute = ['funded_amount','term','int_rate','employment_length'] # columns with a small amount of null values to be imputed
+   columns_to_drop_null_value_rows = ['last_payment_date','last_credit_pull_date','collections_12_mths_ex_med'] # columns with < 1% null values, can drop rows with null values 
    loan_payments_df_copy = loan_payments_df.copy() #create copy of dataframe before removing any values 
-   remove_null = DataFrameTransform(loan_payments_df_copy)
+
+   remove_null = DataFrameTransform(loan_payments_df_copy) # initialises an instance of the DataFrameTransform class 
    for i in range(0, len(columns_to_drop)):
-      loan_payments_df_copy = remove_null.drop_column(columns_to_drop[i])
-   column_names_df_copy = loan_payments_df_copy.columns.tolist()
-   print(column_names_df_copy)
-   #loan_payments_df_copy.info()
-   print(loan_payments_df_copy.head(5))
+      loan_payments_df_copy = remove_null.drop_column(columns_to_drop[i]) # drops columns with > 50 % null values
+   for i in range(0, len(columns_to_drop_null_value_rows)):
+      loan_payments_df_copy = remove_null.drop_null_rows(columns_to_drop_null_value_rows[i]) # drops rows with null values in specific columns
+    
+   
+   my_instance_copy = DataFrameInfo(loan_payments_df_copy) # initialises an instnce of the class 
+
+   column_names_copy = loan_payments_df_copy.columns.tolist() # creates a list of the column headings as strings 
+   null_columns_copy = []
+   for i in range (0, len(column_names_copy)): # to check that the correct columns and rows have been dropped 
+      null_pc = my_instance_copy.null_percentage(column_names_copy[i])
+      print(column_names_copy[i], null_pc)
+      if null_pc > 0.0:
+        null_columns_copy.append(column_names_copy[i]) # append columns with null values to list 
+   print(null_columns_copy)
+   
+   
+
 
    
    
-# drop columns where null values > 50%
