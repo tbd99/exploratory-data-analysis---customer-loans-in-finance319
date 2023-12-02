618f8ef5676f7666fa25dbf9e018314d844dc70f
Tiger Davies <tigerdavies@Tigers-Air.cable.virginm.net>
2023-12-02 12:01:40 +0000

    Calculates % of charged off loans and the amount paid to these loans before being charged off

--- db_data_analysis.py
+++ db_data_analysis.py
@@ -12,7 +12,7 @@ loan_payments_df = pd.read_pickle('cleaned_data.pickle')
 # double check data is correct 
 loan_payments_df.info()
 cleaned_data_plotter_instance = Plotter(loan_payments_df)   
-cleaned_data_plotter_instance.plot_corr_matrix()
+#cleaned_data_plotter_instance.plot_corr_matrix()
 column_names = loan_payments_df.columns.tolist() 
 #for i in range(0, len(column_names)):
     #cleaned_data_plotter_instance.plot_KDE(column_names[i])
@@ -21,12 +21,22 @@ column_names = loan_payments_df.columns.tolist()
 out_prncp_zeros = (loan_payments_df.out_prncp == 0.00).sum()  
 loans_recov_against_total_funding = (out_prncp_zeros/(len(loan_payments_df)))*100
 print(f"{loans_recov_against_total_funding.round(2)} %")
-
-# out_prncp_inv is same % as columns have 1:1 correlation?
-loans_recov_against_inv_funding = loans_recov_against_total_funding 
+loans_recov_against_inv_funding = loans_recov_against_total_funding # out_prncp_inv is same % as columns have 1:1 correlation
 funding = ['total_funding','investor_funding']
 recovery_pc = [loans_recov_against_total_funding, loans_recov_against_inv_funding]
 funding_df = pd.DataFrame({'Funding': funding, 'Recovery_percentage': recovery_pc})
-plt.bar(funding_df['Funding'], funding_df['Recovery_percentage'], width=0.8)
-plt.xticks(rotation=90)
-plt.show() 
+#plt.bar(funding_df['Funding'], funding_df['Recovery_percentage'], width=0.8)
+#plt.xticks(rotation=90)
+#plt.show() 
+
+# Calculate the percentage of charged off loans historically 
+charged_off_loans = (loan_payments_df.loan_status == 'Charged Off').sum() 
+charged_off_loans_pc = (charged_off_loans/(len(loan_payments_df)))*100
+print(f"{charged_off_loans_pc.round(2)} %")
+
+# and the total amount that was paid towards these loans before being charged off.
+charged_off_loans_df = loan_payments_df.loc[loan_payments_df['loan_status']=='Charged Off']
+total_loan_amount = charged_off_loans_df['loan_amount'].sum()
+total_amount_paid = charged_off_loans_df['total_payment'].sum()
+total_amount_paid_pc = (total_amount_paid/total_loan_amount)*100
+print(f"{total_amount_paid_pc.round(2)} %")
