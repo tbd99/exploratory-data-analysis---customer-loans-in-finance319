3ab117c4f9f42165df8fb36e059d36e48b7adb58
Tiger Davies <tigerdavies@MacBook-Air.cable.virginm.net>
2024-01-10 12:32:53 +0000

    removes duplicate code already contained in the ipynb file

--- db_datatransform.py
+++ db_datatransform.py
@@ -54,38 +54,3 @@ class DataTransform():
         self.dataframe[column_name] = (self.dataframe[column_name]).astype('category')
         return self.dataframe
 
-if __name__ == "__main__": # guard added to ensure the game only runs when the script is executed directly 
-   
-   filename = 'loan_payments.csv'
-   loan_payments_df = read_csv(filename)
-   col_to_convert_to_datetime = ['issue_date', 
-                                 'earliest_credit_line', 
-                                 'last_payment_date', 
-                                 'next_payment_date', 
-                                 'last_credit_pull_date'] # list of strings specifying columns to be converted
-   col_to_convert_to_float = ['term', 'employment_length']
-   col_to_convert_to_str = ['grade', 'sub_grade']
-   col_to_convert_to_categorical = ['home_ownership', 
-                                    'verification_status',
-                                    'loan_status', 
-                                    'payment_plan', 
-                                    'purpose', 
-                                    'application_type']  
-   
-   my_instance = DataTransform(loan_payments_df) #initialise instance of class
-   
-   for i in range(0,len(col_to_convert_to_datetime)): #loops over list of column names
-       datetime_format = '%b-%Y'
-       my_instance.obj_to_datetime(col_to_convert_to_datetime[i],datetime_format) #calls the obj_to_datetime method of the DataTransform class to convert data
-
-   for i in range(0,len(col_to_convert_to_float)): #loops over list of column names
-       my_instance.obj_to_int(col_to_convert_to_float[i])
-
-   for i in range(0,len(col_to_convert_to_str)): #loops over list of column names
-       my_instance.obj_to_str(col_to_convert_to_str[i])
-
-   for i in range(0,len(col_to_convert_to_categorical)): #loops over list of column names
-       my_instance.obj_to_str(col_to_convert_to_categorical[i])
-   loan_payments_df.info()
-   save_to_csv(loan_payments_df,'loan_payments_transformed.csv')
-
